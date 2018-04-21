d29fc4f2c70f7c284870c88b9d348a49189266d9
Gil Emerson <gemerson@gilemerson.ca>
2018-04-20 21:48:17 -0400

    LambdaExpressionsEx3 Added as well as LambdaExpressionsEx4 classes

--- src/views/LambdaExpressionsEx4.java
+++ src/views/LambdaExpressionsEx4.java
@@ -7,4 +7,27 @@ package views;
  */
 public class LambdaExpressionsEx4 {
     
+    interface MyString {
+	String myStringFunction(String str);
+    }
+
+public static String reverseStr(MyString reverse, String str){
+  return reverse.myStringFunction(str);
+    }
+
+public static void main (String args[]) {
+	// Block lambda to reverse string
+	MyString reverse = (str) -> {
+		String result = "";
+		
+		for(int i = str.length()-1; i >= 0; i--)
+			result += str.charAt(i);
+		
+		return result;
+	};
+
+	// Output: the string in reverse
+	System.out.println(reverseStr(reverse, "Gil Emerson")); 
+    }
 }
+
